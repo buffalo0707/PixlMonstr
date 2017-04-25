1a599f32317f3b7c69c7ef6169718acc1b73efd1
buffalo0707 <alan.henderson@libertymutual.com>
2017-04-25 09:37:22 -0400

    Adds handlebars template for get monsters
    It does it and it is cool.

--- assets/scripts/game/ui.js
+++ assets/scripts/game/ui.js
@@ -1,8 +1,14 @@
 'use strict'
 
 const store = require('../store.js')
+const showMonsterTemplate = require('../templates/monsters.handlebars')
 
 const getMonstersSuccess = function (data) {
+  console.log('monster data is:', data)
+  const showMonsterHTML = showMonsterTemplate({ monsters: data.monsters })
+  console.log('showMonsterHTML is', showMonsterHTML);
+  $('.content').append(showMonsterHTML)
+  // let showMonstersHtml = showMonstersTemplate({ monsters: data.monsters });
 }
 
 const getMonstersFailures = function () {
@@ -10,7 +16,7 @@ const getMonstersFailures = function () {
 }
 
 const createMonsterSuccess = function (data) {
-console.log(data)
+  console.log(data)
 }
 
 const createMonsterFailure = function () {
